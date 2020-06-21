f89d723cfd48bc239e805e630e85855c6203a79b
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-06-21 20:21:12 -0300

    DTO in book controller

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -5,98 +5,49 @@ using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
+using System.Linq.Expressions;
 using System.Web.Http;
 using System.Web.Http.Description;
 using BooksAPI.Models;
+using BooksAPI.DTOs;
+using System.Threading.Tasks;
 
 namespace BooksAPI.Controllers
 {
+    [RoutePrefix("api/books")]
     public class BooksController : ApiController
     {
         private BooksAPIContext db = new BooksAPIContext();
 
+        // Typed lambda expression for Select() method. 
+        private static readonly Expression<Func<Book, BookDto>> AsBookDto =
+            x => new BookDto
+            {
+                Title = x.Title,
+                Author = x.Author.Name,
+                Genre = x.Genre
+            };
+
+        [Route("")]
         // GET: api/Books
-        public IQueryable<Book> GetBooks()
+        public IQueryable<BookDto> GetBooks()
         {
-            return db.Books;
+            return db.Books.Include(b => b.Author).Select(AsBookDto);
         }
 
+        [Route("{id:int}")]
         // GET: api/Books/5
-        [ResponseType(typeof(Book))]
-        public IHttpActionResult GetBook(int id)
+        [ResponseType(typeof(BookDto))]
+        public async Task<IHttpActionResult> GetBook(int id)
         {
-            Book book = db.Books.Find(id);
+            BookDto book = await db.Books.Include(b => b.Author)
+                .Where(b => b.BookId == id)
+                .Select(AsBookDto)
+                .FirstOrDefaultAsync();
             if (book == null)
             {
                 return NotFound();
             }
-
-            return Ok(book);
-        }
-
-        // PUT: api/Books/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutBook(int id, Book book)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != book.BookId)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(book).State = EntityState.Modified;
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!BookExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/Books
-        [ResponseType(typeof(Book))]
-        public IHttpActionResult PostBook(Book book)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.Books.Add(book);
-            db.SaveChanges();
-
-            return CreatedAtRoute("DefaultApi", new { id = book.BookId }, book);
-        }
-
-        // DELETE: api/Books/5
-        [ResponseType(typeof(Book))]
-        public IHttpActionResult DeleteBook(int id)
-        {
-            Book book = db.Books.Find(id);
-            if (book == null)
-            {
-                return NotFound();
-            }
-
-            db.Books.Remove(book);
-            db.SaveChanges();
 
             return Ok(book);
         }
@@ -108,11 +59,6 @@ namespace BooksAPI.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
-        }
-
-        private bool BookExists(int id)
-        {
-            return db.Books.Count(e => e.BookId == id) > 0;
         }
     }
 }
